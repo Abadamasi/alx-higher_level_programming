235583532b8783f36b2d1918f4ea116d53107999
Abadamasi <abbabadamasi33@gmail.com>
2023-08-14 22:44:23 +0000

    fixing bugs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
-=======
->>>>>>> b816f1cf2d867579842082214c95441cc173556e
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
@@ -11,9 +7,4 @@ def element_at(my_list, idx):
 
     if idx > length - 1:
         return (None)
-
-<<<<<<< HEAD
-return(my_list[idx])
-=======
-    return(my_list[idx])
->>>>>>> b816f1cf2d867579842082214c95441cc173556e
+	return(my_list[idx])
