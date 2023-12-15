a6d12377e1d7a3e24f94aeae3b0c8d68e44ecdf9
Abadamasi <abbabadamasi33@gmail.com>
2023-12-15 07:37:40 -0800

    first table

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,9 @@
 -- This is a script that lists all the tables of a database in your MySQL server.
-SHOW TABLES;
+USE mysql;
+-- create first_table table
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	column1 VARCHAR(256),
+	column2 INT, 
+	column3 DATE
+);
