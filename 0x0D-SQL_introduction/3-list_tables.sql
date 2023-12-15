72e0f389a2d7cccff2823acab118b280d9c95768
Abadamasi <abbabadamasi33@gmail.com>
2023-12-15 07:38:39 -0800

    first table

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -3,7 +3,5 @@ USE mysql;
 -- create first_table table
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	column1 VARCHAR(256),
-	column2 INT, 
-	column3 DATE
+	column1 VARCHAR(256)
 );
