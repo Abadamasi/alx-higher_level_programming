0d08c68f9991f50d885a8e6f88518f250a7d13b8
Abadamasi <abbabadamasi33@gmail.com>
2024-01-03 14:59:31 -0800

    languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstline = 'C is fun';
+
+const secondline = 'Python is cool';
+
+const thirdline = 'JavaScript is amazing';
+console.log('C is fun, Python is colle, JavaScript is amazing');
