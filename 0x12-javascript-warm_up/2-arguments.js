e05f17a3435b8685106de7b2115dee26903b8135
Abadamasi <abbabadamasi33@gmail.com>
2024-01-03 15:34:21 -0800

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #1/usr/bin/node
-const count = process.argvv.length;
-console.log(count === 0 ? 'No argument' : count === 1 ? 'Argument found' : 'Arguments found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
 }
