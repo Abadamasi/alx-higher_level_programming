8cf4174474724dfcbc568f040bafc5044addd840
Abadamasi <abbabadamasi33@gmail.com>
2024-01-07 14:05:54 -0800

    Languages_loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const languages of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	console.log(language);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+	console.log(line);
 }
